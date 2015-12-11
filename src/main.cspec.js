9dd1ad34ecbc8c3cd732fd95edc7121bf23ac5ae
Kris Siegel <krissiegel@gmail.com>
2015-12-11 03:52:33 -0500

    First unit test to get it to function. Haha.

---
+++ src/main.cspec.js
@@ -0,0 +1,8 @@
+var expect = chai.expect;
+describe("./main.js", function() {
+    "use strict";
+
+    it("Verify that BoPattern exists", function() {
+        expect(window.BoPattern).to.exist;
+    });
+});
