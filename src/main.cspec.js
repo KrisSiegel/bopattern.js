8f0fcf2516f5198d9aec65753440391a003f6225
Kris Siegel <KrisSiegel@Gmail.com>
2015-12-11 22:38:50 -0500

    More unit tests Fixed spacing in package.json

--- src/main.cspec.js
+++ src/main.cspec.js
@@ -2,7 +2,24 @@ var expect = chai.expect;
 describe("./main.js", function() {
     "use strict";
 
-    it("Verify that BoPattern exists", function() {
+    it("BoPattern() - Verify that BoPattern exists", function() {
         expect(window.BoPattern).to.exist;
+    });
+
+    it("BoPattern(selector) - Finds or creates a canvas element based on a selector", function() {
+        var bopat = BoPattern("body");
+        expect(bopat).to.exist;
+        bopat.debug = true;
+        expect(bopat.internal.canvas).to.exist;
+        bopat.debug = false;
+
+        bopat.removeSelf();
+    });
+
+    it("BoPattern(invalid) - Throws an exception when bad input is supplied", function() {
+        expect(BoPattern.bind(12)).to.throw;
+        expect(BoPattern.bind(null)).to.throw;
+        expect(BoPattern.bind(undefined)).to.throw;
+        expect(BoPattern.bind(new Date())).to.throw;
     });
 });
